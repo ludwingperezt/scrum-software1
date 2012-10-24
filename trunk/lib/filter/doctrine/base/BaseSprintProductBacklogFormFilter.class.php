71ce2b905324daacc642a524e38b9fa8d4943ab1
mirage232@gmail.com <mirage232@gmail.com@10f64ff8-43de-8fd1-13de-d6fe25b47af4>
2012-10-24 04:46:20 +0000

    Aplicada la modificación de la tabla sprintproductbacklog al proyecto. Se modificó el schema.yml para agregar las modificaciones a la tabla. Solo cambié el modulo de sprintproductbacklog Los archivos base a excepcion de la tabla sprintproductbacklog, todos son iguales a la version que subió Mario.

--- trunk/lib/filter/doctrine/base/BaseSprintProductBacklogFormFilter.class.php
+++ trunk/lib/filter/doctrine/base/BaseSprintProductBacklogFormFilter.class.php
@@ -13,11 +13,19 @@ abstract class BaseSprintProductBacklogFormFilter extends BaseFormFilterDoctrine
   public function setup()
   {
     $this->setWidgets(array(
+      'Persona_id'        => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Persona'), 'add_empty' => true)),
+      'Sprint_id'         => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Sprint'), 'add_empty' => true)),
+      'ProductBacklog_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('ProductBacklog'), 'add_empty' => true)),
+      'Anotaciones'       => new sfWidgetFormFilterInput(),
       'created_at'        => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'updated_at'        => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
     ));
 
     $this->setValidators(array(
+      'Persona_id'        => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Persona'), 'column' => 'id')),
+      'Sprint_id'         => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('Sprint'), 'column' => 'id')),
+      'ProductBacklog_id' => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('ProductBacklog'), 'column' => 'id')),
+      'Anotaciones'       => new sfValidatorPass(array('required' => false)),
       'created_at'        => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
       'updated_at'        => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
     ));
@@ -39,8 +47,11 @@ abstract class BaseSprintProductBacklogFormFilter extends BaseFormFilterDoctrine
   public function getFields()
   {
     return array(
-      'Sprint_id'         => 'Number',
-      'ProductBacklog_id' => 'Number',
+      'id'                => 'Number',
+      'Persona_id'        => 'ForeignKey',
+      'Sprint_id'         => 'ForeignKey',
+      'ProductBacklog_id' => 'ForeignKey',
+      'Anotaciones'       => 'Text',
       'created_at'        => 'Date',
       'updated_at'        => 'Date',
     );
