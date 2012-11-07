1ee5b337ec39ba19ad673e4eb607e74330ccf117
MarioRValdes <MarioRValdes@10f64ff8-43de-8fd1-13de-d6fe25b47af4>
2012-11-07 07:58:30 +0000

    Arregle diseño de la tabla en el módulo de reuniones!

--- trunk/apps/frontend/modules/Acceso/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/Acceso/templates/indexSuccess.php
@@ -1,1 +1,12 @@
-Aca se muestran las novedades...
+	<h1>MENU TEMPORAL</h1>
+	<a href="<?php echo url_for('@default?module=persona')?>">Personas</a><br>
+	<a href="<?php echo url_for('@default?module=proyecto')?>">Proyectos</a><br>
+	<a href="<?php echo url_for('@default?module=reunion')?>">Reuniones</a><br>
+	<a href="<?php echo url_for('@default?module=equipo')?>">Equipos</a><br>
+	<a href="<?php echo url_for('@default?module=historialchat')?>">Chat</a><br>
+	<a href="<?php echo url_for('@default?module=link')?>">Links</a><br>
+	<a href="<?php echo url_for('@default?module=productbacklog')?>">Product Backlog</a><br>
+	<a href="<?php echo url_for('@default?module=sprint')?>">Sprints</a><br>
+	<a href="<?php echo url_for('@default?module=sprintproductbacklog')?>">Sprint Backlog</a><br>
+	<a href="<?php echo url_for('@default?module=tarea')?>">Tareas</a><br>
+	<a href="<?php echo url_for('@default?module=cambio')?>">Cambios</a><br>
