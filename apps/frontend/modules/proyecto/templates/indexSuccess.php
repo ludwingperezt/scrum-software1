a97b965464a474555d209ef6502fbb3ee1dca759
lindacor23 <lindacor23@10f64ff8-43de-8fd1-13de-d6fe25b47af4>
2012-11-08 05:26:38 +0000

    estandarizacion (sort, tfoot)

--- apps/frontend/modules/proyecto/templates/indexSuccess.php
+++ apps/frontend/modules/proyecto/templates/indexSuccess.php
@@ -1,12 +1,12 @@
 <h1>Lista de Proyectos</h1>
 
-<table>
+<table id="demoTable" style="border: 1px solid #ccc;" cellspacing="0" width="800" align="center">
   <thead>
     <tr>
       <th></th>
-      <th>Nombre</th>
-      <th>Persona</th>      
-      <th>Descripcion</th>
+      <th sort="Nombre">Nombre</th>
+      <th sort="Persona">Persona</th>      
+      <th sort="Descripcion">Descripcion</th>
       <th>Comentarios</th>
       <!--<th>Created at</th>
       <th>Updated at</th>
@@ -35,6 +35,16 @@
     </tr>
     <?php endforeach; ?>
   </tbody>
+  <tfoot class="nav">
+		<tr>
+			<td colspan=7>
+				<div class="pagination"></div>
+				<div class="paginationTitle">Pagina</div>
+				<div class="selectPerPage"></div>
+				<div class="status"></div>
+			</td>
+		</tr>
+	</tfoot>
 </table>
-
+<br>
   <a href="<?php echo url_for('proyecto/new') ?>">Crear Proyecto</a>
