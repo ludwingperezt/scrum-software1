64356bb17e5be632ba610a3479c9482da5a49a79
lindacor23 <lindacor23@10f64ff8-43de-8fd1-13de-d6fe25b47af4>
2012-11-08 05:39:01 +0000

    estandarizacion (sort, tfoot)

--- trunk/apps/frontend/modules/equipo/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/equipo/templates/indexSuccess.php
@@ -1,13 +1,13 @@
 <?php use_stylesheet('main.css');?>
 <h1>Miembros del equipo</h1>
-<table class="tablas">
+<table id="demoTable" style="border: 1px solid #ccc;" cellspacing="0" width="800" align="center">
   <thead>
     <tr>
-      <th>Nombre</th>
-      <th>Rol</th>
-      <th>Anotación</th>
-      <th>Fecha de agregación al proyecto</th>
-      <th>Fecha de última modificación</th>
+      <th sort="Nombre">Nombre</th>
+      <th sort="Rol">Rol</th>
+      <th sort="Anotacion">Anotación</th>
+      <th sort="Fecha de agregacion al proyecto">Fecha de agregación al proyecto</th>
+      <th sort="Fecha de ultima modificacion">Fecha de última modificación</th>
 	  <th>     </th>
     </tr>
   </thead>
@@ -28,6 +28,16 @@
     </tr>
     <?php endforeach; ?>
   </tbody>
+  <tfoot class="nav">
+		<tr>
+			<td colspan=7>
+				<div class="pagination"></div>
+				<div class="paginationTitle">Pagina</div>
+				<div class="selectPerPage"></div>
+				<div class="status"></div>
+			</td>
+		</tr>
+	</tfoot>
 </table>
-
+<br>
   <a href="<?php echo url_for('equipo/new') ?>">Agregar miembro</a>
