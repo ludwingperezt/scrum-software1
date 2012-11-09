25cee18d4cadc2b2bed1bb2fe87aca7cd5696609
mirage232 <mirage232@gmail.com>
2012-11-09 07:20:33 +0000

    backend

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -1,15 +1,51 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
+    <title>Consola de administración de scrum manager</title>
     <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
+    <?php use_stylesheet('admin.css') ?>
     <?php include_javascripts() ?>
+    <?php include_stylesheets() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
-	<li><?php echo link_to('Logout', 'sf_guard_signout') ?></li>
+    <div id="container">
+      <div id="header">
+        <h1>
+          <a href="<?php echo url_for('homepage') ?>">
+            <img src="/legacy/images/logo.jpg" alt="Jobeet Job Board" />
+          </a>
+        </h1>
+      </div>
+ 
+      <div id="menu">
+        <ul>
+          <li>
+            <?php echo link_to('rol', 'rol') ?>
+          </li>
+          <li>
+            <?php echo link_to('estado', 'estado') ?>
+          </li>
+	  <li>
+            <?php echo link_to('Grupos', 'sf_Guard_Group') ?>
+          </li>
+ 	  <li>
+            <?php echo link_to('Permisos', 'sf_Guard_Permission') ?>
+          </li>
+          <li>
+            <?php echo link_to('Salir', 'sf_guard_signout') ?>
+          </li>
+        </ul>
+      </div>
+ 
+      <div id="content">
+        <?php echo $sf_content ?>
+      </div>
+ 
+      <div id="footer">
+        <img src="/legacy/images/jobeet-mini.png" />
+        powered by <a href="/">
+        <img src="/legacy/images/symfony.gif" alt="symfony framework" /></a>
+      </div>
+    </div>
   </body>
 </html>
